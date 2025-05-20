8c938ec06563d9806ff80761209fab3f2ed1a8d6
Youssef Mahdoubi <123659397+ysfmh14@users.noreply.github.com>
2025-05-20 21:32:20 +0100

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,7 +1,7 @@
-POST_TRAINING_DATA_FILE = "POST_TRAINING_DATA_FILE.xlsx" #  file must be .xlsx
-FINE_TUNING_DATA_FILE = "FINE_TUNING_DATA_FILE.xlsx" #  file must be .xlsx
-LEXIC_FILE = "LEXIC_FILE.xlsx" # The file must be .xlsx
-LABEL_NUM = 2 # Number of labels in fine tuning data
-MODEL_NAME = "bert-base-uncased" # name of the langage modele 
-USED_STRATEGY = 1 # If you want to use strategie 1 write 1. If you want to use strategie 2 write 2.
-MAX_LENGTH_TEXT = 512 # Must be <= 512
+POST_TRAINING_DATA_FILE = "POST_TRAINING_DATA_FILE.xlsx"         #  file must be .xlsx
+FINE_TUNING_DATA_FILE = "FINE_TUNING_DATA_FILE.xlsx"             #  file must be .xlsx
+LEXIC_FILE = "LEXIC_FILE.xlsx"         # The file must be .xlsx
+LABEL_NUM = 2                # Number of labels in the fine-tuning data
+MODEL_NAME = "bert-base-uncased"  # Name of the language model
+USED_STRATEGY = 1           # Choose strategy: 1 for strategy 1, 2 for strategy 2
+MAX_LENGTH_TEXT = 512       # Maximum text length (must be ≤ 512)
